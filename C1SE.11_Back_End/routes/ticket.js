bd8a80ee6f849c58c2460eb523e5a4e0735345c8
NguyenHoangQuocAnh <nguyenhoangquocanh1012@gmail.com>
2023-10-30 02:39:34 +0700

    Update Datve for UI customer 30-10

--- C1SE.11_Back_End/routes/ticket.js
+++ C1SE.11_Back_End/routes/ticket.js
@@ -2,8 +2,12 @@ const express = require('express');
 const TicketController = require('../app/controller/TicketController');
 const router = express.Router();
 
-router.get('/getQR', async (req, res) => {
+router.get('/getQR/:id', async (req, res) => {
     TicketController.generateQRCode(req, res);
+});
+
+router.get('/getTicket/:id', async (req, res) => {
+    TicketController.getTicket(req, res);
 });
 
 module.exports = router;
