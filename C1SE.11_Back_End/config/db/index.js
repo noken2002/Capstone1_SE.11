9887bfa442f414513134c8f9a6aac534132b06bc
NguyenThanhHung1210 <thanhhung12102001@gmail.com>
2023-11-01 23:08:14 +0700

    Add DriverUI

--- C1SE.11_Back_End/config/db/index.js
+++ C1SE.11_Back_End/config/db/index.js
@@ -5,16 +5,20 @@ const dotenv = require('dotenv');
 // get config vars
 dotenv.config();
 
-const MSSQLpool = new mssql.ConnectionPool({
-    server: "QUOCANH-PC\\SQLEXPRESS01",
-    database: "expressTickets",
-    driver: 'msnodesqlv8',
-    options: {
-        trustedConnection: true
-    }
-});
+// const MSSQLpool = new mssql.ConnectionPool({
+//     server: "QUOCANH-PC\\SQLEXPRESS01",
+//     database: "expressTickets",
+//     driver: 'msnodesqlv8',
+//     options: {
+//         trustedConnection: true
+//     }
+// });
 
-// const MSSQLpool = new mssql.ConnectionPool('Driver={ODBC Driver 17 for SQL Server};Server=QUOCANH-PC\SQLEXPRESS;Database=expressTickets;Trusted_Connection=yes;');
+// const MSSQLpool = new mssql.ConnectionPool('Driver={ODBC Driver 17 for SQL Server};Server=DESKTOP-QORQO49\SQLEXPRESS;Database=expressTickets;Trusted_Connection=yes;');
+
+const MSSQLpool = new mssql.ConnectionPool({
+    connectionString: 'Driver={ODBC Driver 17 for SQL Server};Server=DESKTOP-QORQO49\\SQLEXPRESS;Database=expressTickets;Trusted_Connection=yes;',
+});
 
 MSSQLpool.connect().then(() => {
     console.log('Connected to mssql database');
